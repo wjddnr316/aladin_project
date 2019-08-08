e585cd1850abbab9e8a1e8676b62d49f7d3aeaea
jeonguk seo <wjddnr316@naver.com>
2019-08-09 01:05:41 +0900

    네비게이션(js오버반응 까지 완료)

--- js/custom.js
+++ js/custom.js
@@ -10,3 +10,29 @@ function topten(){
     $("#top10").addClass("on");
     $("#top5").removeClass("on");
 };
+
+$(document).ready(function(){
+    
+    $(".main_nav,.inner_contain").mouseenter(function(){
+      $(".inner_contain").addClass("on"); 
+    });
+    $(".main_nav,.inner_contain").mouseleave(function(){
+        $(".inner_contain").removeClass("on");
+    });
+    
+    var mainNav = $(".main_nav li");
+    $(mainNav).mouseenter(function(){
+        var idx = $(this).index();
+        var dropMenu = $(".drop_menu");
+        
+        $(dropMenu).removeClass("on");
+        $(dropMenu[idx]).addClass("on");
+        
+        $(dropMenu).mouseenter(function(){
+            $(dropMenu).removeClass("on");
+            $(this).addClass("on"); 
+        });
+    });
+});
+
+    
