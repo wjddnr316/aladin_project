6ba2c9ac475c9fa73b6871850dff3e5e723ec8ce
jeonguk seo <wjddnr316@naver.com>
2019-08-08 13:37:24 +0900

    aladin프로젝트 시작

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+function topfive(){
+    $(".tapbtn").removeClass("on");
+    $(".tapbtn1").addClass("on");
+    $("#top5").addClass("on");
+    $("#top10").removeClass("on");
+};
+function topten(){
+    $(".tapbtn").removeClass("on");
+    $(".tapbtn2").addClass("on");
+    $("#top10").addClass("on");
+    $("#top5").removeClass("on");
+};
